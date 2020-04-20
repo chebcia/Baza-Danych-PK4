c96fdd415973cb67629fbf386c9d3aa89f5aa9e7
chebcia <43813992+chebcia@users.noreply.github.com>
2020-04-20 19:05:55 +0200

    Update ZarzadzanieLekami.h

--- ZarzadzanieLekami.h
+++ ZarzadzanieLekami.h
@@ -5,7 +5,61 @@
 class ZarzadzanieLekami {
 
 public:
-	Listalekow* znajdzLekarstwa(Lek lekarstwo, bool czySzukacPoRefundacji);
+	Listalekow* znajdzLekarstwa(Lek lekarstwo, bool czySzukacPoRefundacji)
+	{
+		string nazwaKategorii;
+		cout << "podaj kategorie leku (nic by nie korzystac z pola): ";
+		getline(cin, nazwaKategorii);
+		vector<string> listaKategorii;
+		Listalekow* listaZnalezionychLekow = NULL;
+
+		if (nazwaKategorii.compare("") != 0)
+		{
+			listaKategorii.push_back(nazwaKategorii);
+		}
+		else
+
+		{
+			fstream plik;
+			// sprawdzenie fragmentu instrukcją try
+			try
+			{
+				plik.open("Kategorie.txt", std::ios::in);
+				if (!plik.good())
+				{
+					throw std::exception("Nie ");
+				}
+				while (!plik.eof())
+				{
+					string napis;
+
+
+					getline(plik, napis);
+					listaKategorii.push_back(napis);
+
+				}
+			}
+			catch (const std::exception &e)
+			{
+				std::cout << std::endl << e.what() << std::endl;
+				plik.close();
+			}
+		}
+
+
+
+		std::vector<std::thread> watki(listaKategorii.size());
+		for (int a = 0; a < listaKategorii.size(); a++)
+		{
+			string tmp = listaKategorii[a];
+			watki[a] = std::thread(szukajPoKategorii, lekarstwo, tmp, std::ref(listaZnalezionychLekow), czySzukacPoRefundacji);
+		}
+		for (int a = 0; a < listaKategorii.size(); ++a)
+		{
+			watki[a].join();
+		}
+		return listaZnalezionychLekow;
+	}
 	Listalekow* znajdzLekarstwa();
 
 
