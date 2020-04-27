eddae27d4026800a7ce34938ff3c7899df849ed7
chebcia <43813992+chebcia@users.noreply.github.com>
2020-04-28 00:57:55 +0200

    Update Admin.cpp

--- Admin.cpp
+++ Admin.cpp
@@ -1,42 +1,19 @@
 #include "Admin.h"
-
-
-
-
-void Admin::dodajUzytkownika()
-{
-
-	cout << "Dodaj nowego uzytkownika, podaj login, haslo i role" << endl;
-	string login;
-	string haslo;
-	string rola;
-	cout << "Login: "<<endl;
-	cin >> login;
-	cout << "Haslo: "<<endl;
-	cin >> haslo;
-	cout << "rola: "<<endl;
-	cin >> rola;
-	dodawaniedopliku(login, haslo, rola);
-}
+#include <QMessageBox>
 
 void Admin::dodawaniedopliku(string login, string haslo, string rola)
 {
-
 	int id = 1;
 	string linia;
 	ifstream plik1("hasla.txt");
 
 	if (plik1)
-	{
-		int aktualneID;
-		while (getline(plik1, linia))
-		{
-
-			if (atoi(linia.c_str()) > id)
-			{
-				id = atoi(linia.c_str());
-			}
-		}
+    {
+        while (getline(plik1, linia))
+        {
+            if (atoi(linia.c_str()) > id)
+                id = atoi(linia.c_str());
+        }
 		plik1.close();
 	}
 	id++;
@@ -44,18 +21,16 @@ void Admin::dodawaniedopliku(string login, string haslo, string rola)
 	try
 	{
 		plik << id << " " << login << " " << haslo << " " << rola << '\n';
-		plik.close(); //obowiązkowo należy zamknąć plik
-		menu();
+        plik.close(); //obowiązkowo należy zamknąć plik
 	}
 	catch (exception e)
 	{
-		cout << "Blad podczas dostepu do bazy uzytkownikow" << endl;
-		plik.close();
+        plik.close();
+        QMessageBox::critical(nullptr, "BazyDanych", (string("Blad podczas dostepu do bazy uzytkownikow\n") + string(e.what())).c_str());
 	}
-
 }
 
-void Admin::usunUzytkownika2(string login)
+void Admin::usunUzytkownika(string login)
 {
 	fstream plik("hasla.txt", std::ios::in);
 	try
@@ -64,7 +39,8 @@ void Admin::usunUzytkownika2(string login)
 		string loginzpliku;
 		// użycie vectora jako kontenera stl
 		std::vector<string> liniezpliku;
-		while (getline(plik, linia)) {
+        while (getline(plik, linia))
+        {
 			char * schowek;
 			char* skonwertowany = new char[linia.length() + 1];
 			strcpy(skonwertowany, linia.c_str());
@@ -72,87 +48,26 @@ void Admin::usunUzytkownika2(string login)
 
 			//druga kolumna, login
 			schowek = strtok(NULL, " ");
-
-			cout << schowek << endl;
 			loginzpliku = schowek;
 
-			if (login != loginzpliku) {
-				liniezpliku.push_back(linia + "\n");
-
-			}
-			delete skonwertowany;
+            if (login != loginzpliku)
+                liniezpliku.push_back(linia + "\n");
+            delete[] skonwertowany;
 		}
 		plik.close();
 		plik.open("hasla.txt", std::ofstream::out | std::ofstream::trunc);
-		if (plik.good()) {
+        if (plik.good())
+        {
 			// iterator stl 
 			std::vector<string>::iterator cell = liniezpliku.begin();
-			for (cell; cell != liniezpliku.end(); cell++) {
-
-				plik << *cell;
-			}
+            for (; cell != liniezpliku.end(); cell++)
+                plik << *cell;
 		}
 		plik.close();
-
 	}
 	catch (exception e)
-	{
-		cout << "Blad podczas dostepu do bazy uzytkownikow" << endl;
-		plik.close();
-	}
-
-
-
-
-}
-
-
-
-
-void Admin::usunUzytkownika()
-{
-	cout << "Podaj login uzytkownika ktory ma zostac usuniety"<<endl;
-	string login;
-	cout << "Login: "<<endl;
-	cin >> login;
-	usunUzytkownika2(login);
-	
-
-
-}
-
-Admin::Admin()
-{
-	rola = "admin";
-}
-
-void Admin::menu()
-{
-	int opcja = 1;
-	while (opcja != 0)
-	{
-
-		cout << "1.Dodaj uzytkownika" << endl;
-		cout << "2.Usun uzytkownika" << endl;
-		cout << "0.Wyjdz" << endl;
-
-		cin >> opcja;
-
-		switch (opcja)
-		{
-		case 1:
-			dodajUzytkownika();
-			break;
-		case 2:
-			usunUzytkownika();
-			break;
-		case 0:
-			exit(0);
-
-		default:
-			cout << "Bledna opcja" << endl;
-		}
+    {
+        plik.close();
+        QMessageBox::critical(nullptr, "BazyDanych", (string("Blad podczas dostepu do bazy uzytkownikow\n") + string(e.what())).c_str());
 	}
 }
-
-
