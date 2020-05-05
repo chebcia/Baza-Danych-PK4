540041f1cf1972fadaa61be708882e9398de71f0
chebcia <43813992+chebcia@users.noreply.github.com>
2020-05-05 11:09:23 +0200

    Update ZarzadzanieLekami.h

--- ZarzadzanieLekami.h
+++ ZarzadzanieLekami.h
@@ -1,12 +1,10 @@
 #pragma once
 #include "ListaLekow.h"
 #include <string>
-#include <iostream>
-class ZarzadzanieLekami {
 
+class ZarzadzanieLekami
+{
 public:
 	Listalekow* znajdzLekarstwa(Lek lekarstwo, bool czySzukacPoRefundacji);
 	Listalekow* znajdzLekarstwa();
-
-
 };
